60cb2d438c2f7b257282f81e7faaf3caad464671
Dmitriy Kuzmin <slaggery@gmail.com>
2020-09-18 12:31:28 +0700

    test

---
+++ local/js/displayBlock.js
@@ -0,0 +1,5 @@
+BX.ready(function(){
+    console.log('ready')
+})
+
+
--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,1 +1,16 @@
 <?php
+include_once($_SERVER["DOCUMENT_ROOT"] . '/local/js/displayBlock.js');
+
+AddEventHandler('main', 'OnEpilog', function(){
+    $arJsConfig = array(
+        'displayBlock' => array(
+            'js' => '/local/js/displayBlock.js',
+        ),
+    );
+
+    foreach ($arJsConfig as $ext => $arExt) {
+        \CJSCore::RegisterExt($ext, $arExt);
+    }
+
+    CUtil::InitJSCore(array('displayBlock'));
+});
