0ef65b98a25e871eb9e33fc5f360395d3b28cba1
eearzikaev <ee.arzikaev@gmail.com>
2020-09-18 15:02:52 +0700

    update js file

--- local/js/displayBlock.js
+++ local/js/displayBlock.js
@@ -1,5 +1,13 @@
 BX.ready(function(){
     console.log('ready')
+    const elementsByCheck = document.getElementsByName('UF_CRM_1600328889064')
+    console.log(elementsByCheck)
+   // if(elementsByCheck[0] == true){ console.log('hello')}
+    const elementByBlock = document.getElementsByClassName('crm-entity-card-widget')
+    elementByBlock[1].style.display = 'none'
+
+
+
 })
 
 
