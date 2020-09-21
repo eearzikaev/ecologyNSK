7b5187e45cefa9d7f2498b6d6fdb8cf772bc8e84
Dmitriy Kuzmin <slaggery@gmail.com>
2020-09-21 17:32:19 +0700

    21.09

--- local/js/displayBlock.js
+++ local/js/displayBlock.js
@@ -1,8 +1,27 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-    const elementByBlock = document.getElementsByClassName('crm-entity-card-widget')
-    elementByBlock[1].style.display = 'none'
+BX.addCustomEvent('bx.main.filter:blur', (e) => {
+    function clickCheck() {
+        console.log('e', e)
+        let check = document.getElementsByName('UF_CRM_1600328889064')
+        const elementByBlock = document.getElementsByClassName('crm-entity-card-widget')
+        if (!check[1].checked) elementByBlock[1].style.display = 'none'
+        if (check[1].checked) elementByBlock[1].style.display = ''
+        console.log('check', check[1].checked)
+    }
 
-    let allElems = document.querySelectorAll('[data-cid="UF_CRM_1600328889064"]')
+    try {
+        let check = document.getElementsByName('UF_CRM_1600328889064')[1].onclick = clickCheck
+    } catch (e) {
+        console.log('error', e)
+    }
+})
 
-    console.log('allElems', Array.prototype.slice.call(allElems))
-});
+BX.addCustomEvent('bx.crm.entityeditorfield:onlayout', (e) => {
+    if (e._id === 'UF_CRM_1600328889064') {
+        const elementByBlock = document.getElementsByClassName('crm-entity-card-widget')
+        let allElems = document.querySelectorAll('[data-cid="UF_CRM_1600328889064"]')
+        let span = allElems[0].getElementsByClassName('fields boolean field-item')
+
+        if (span[0].innerText === 'нет') elementByBlock[1].style.display = 'none'
+    }
+})
+
